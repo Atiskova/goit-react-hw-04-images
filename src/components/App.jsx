201736b35a5d5fdf5ae3747cc2e3b20d3aa8f212
Anna Atiskova <anna.atiskova@gmail.com>
2023-02-24 02:31:33 +0200

    solution

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { toast } from 'react-toastify';
 
 import { Searchbar } from './Searchbar/Searcbar';
@@ -10,84 +10,153 @@ import { Text } from './Text/Text.styled';
 
 import * as ImageService from '../service/api';
 
-export class App extends React.Component {
-  state = {
-    images: [],
-    page: 1,
-    totalImages: 0,
-    query: '',
-    loading: false,
-    error: '',
-  };
+// export class App extends React.Component {
+//   state = {
+//     images: [],
+//     page: 1,
+//     totalImages: 0,
+//     query: '',
+//     loading: false,
+//     error: '',
+//   };
 
-  componentDidUpdate(_, prevState) {
-    const { query, page, error } = this.state;
-    if (prevState.query !== query || prevState.page !== page) {
-      this.getImages();
-    }
-    if (prevState.error !== error && error) {
-      toast.error(error);
-    }
-  }
+//   componentDidUpdate(_, prevState) {
+//     const { query, page, error } = this.state;
+//     if (prevState.query !== query || prevState.page !== page) {
+//       this.getImages();
+//     }
+//     if (prevState.error !== error && error) {
+//       toast.error(error);
+//     }
+//   }
 
-  getImages = async () => {
-    const { query, page } = this.state;
+//   getImages = async () => {
+//     const { query, page } = this.state;
 
-    try {
-      this.setState({ loading: true });
-      const { images, totalHits } = await ImageService.getImages(query, page);
+//     try {
+//       this.setState({ loading: true });
+//       const { images, totalHits } = await ImageService.getImages(query, page);
 
-      if (images.length === 0) {
-        toast('Sorry, there are no images matching your search query.');
-        return;
+//       if (images.length === 0) {
+//         toast('Sorry, there are no images matching your search query.');
+//         return;
+//       }
+//       this.setState(prevState => ({
+//         images: [...prevState.images, ...images],
+//         error: '',
+//         totalImages: totalHits,
+//       }));
+//     } catch (error) {
+//       toast('Opps🙀, something went wrong, please try again later');
+//     } finally {
+//       this.setState({ loading: false });
+//     }
+//   };
+
+//   getQuery = value => {
+//     if (value === this.state.query) {
+//       toast('Please, change your request');
+//       return;
+//     }
+//     this.setState({
+//       query: value,
+//       images: [],
+//       page: 1,
+//       totalImages: 0,
+//     });
+//   };
+
+//   loadMore = () => {
+//     this.setState(prevState => ({
+//       page: prevState.page + 1,
+//     }));
+//   };
+
+//   render() {
+//     const { images, loading, error, totalImages } = this.state;
+
+//     return (
+//       <div className="app">
+//         <Searchbar onSubmit={this.getQuery}></Searchbar>
+//         {images.length !== 0 && <ImageGallery images={images} />}
+//         {!loading && images.length === 0 && !error && (
+//           <Text>There are no images yet, please make your search 🞁</Text>
+//         )}
+//         {!loading && totalImages !== images.length && (
+//           <Button onClick={this.loadMore} />
+//         )}
+//         {loading && <Loader />}
+//         <Toast />
+//       </div>
+//     );
+//   }
+// }
+
+export const App = () => {
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalImages, setTotalImages] = useState(0);
+  const [query, setQuery] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+
+  useEffect(() => {
+    if (!query)  return;
+
+    (async function () {
+      try {
+        setLoading(true);
+        const { images, totalHits } = await ImageService.getImages(query, page);
+  
+        if (images.length === 0) {
+          toast('Sorry, there are no images matching your search query.');
+          return;
+        }
+  
+        setImages(prevState => [...prevState, ...images]);
+        setError('');
+        setTotalImages(totalHits);
+      } catch (error) {
+        toast('Opps🙀, something went wrong, please try again later');
+      } finally {
+        setLoading(false);
       }
-      this.setState(prevState => ({
-        images: [...prevState.images, ...images],
-        error: '',
-        totalImages: totalHits,
-      }));
-    } catch (error) {
-      toast('Opps🙀, something went wrong, please try again later');
-    } finally {
-      this.setState({ loading: false });
-    }
-  };
+    })();
 
-  getQuery = (value) => {
-    if (value === this.state.query) {
+   }, [query, page])
+
+  
+
+  const getQuery = value => {
+    if (value === query) {
       toast('Please, change your request');
       return;
     }
-    this.setState({
-      query: value,
-      images: [],
-      page: 1,
-      totalImages: 0,
-    });
+
+    setQuery(value);
+    setImages([]);
+    setPage(1);
+    setTotalImages(0);
   };
 
-  loadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+  const loadMore = () => {
+    setPage(prevState => prevState + 1);
   };
 
-  render() {
-    const { images, loading, error, totalImages } = this.state;
+  return (
+    <div className="app">
+      <Searchbar onSubmit={getQuery}></Searchbar>
+      {images.length !== 0 && <ImageGallery images={images} />}
+      {!loading && images.length === 0 && !error && (
+        <Text>There are no images yet, please make your search 🞁</Text>
+      )}
+      {!loading && totalImages !== images.length && (
+        <Button onClick={loadMore} />
+      )}
+      {loading && <Loader />}
+      <Toast />
+    </div>
+  );
 
-    return (
-      <div className="app">
-        <Searchbar onSubmit={this.getQuery}></Searchbar>
-        {images.length !== 0 && <ImageGallery images={images} />}
-        {!loading && images.length === 0 && !error && (
-          <Text>There are no images yet, please make your search 🞁</Text>
-        )}
-        {!loading && totalImages !== images.length && (
-          <Button onClick={this.loadMore} />
-        )}
-        {loading && <Loader />}
-        <Toast />
-      </div>
-    );
-  }
-}
+};
--- src/components/Searchbar/Searcbar.jsx
+++ src/components/Searchbar/Searcbar.jsx
@@ -1,42 +1,75 @@
 // import PropTypes from 'prop-types';
 import { FiSearch } from 'react-icons/fi';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Header, SearchForm, Button, Input } from './Searchbar.styled';
 
-export class Searchbar extends React.Component {
-  state = {
-    value: '',
+// export class Searchbar extends React.Component {
+//   state = {
+//     value: '',
+//   };
+
+//   handleChange = event => {
+//     this.setState({ value: event.target.value });
+//   };
+
+//   handleSubmit = event => {
+//     event.preventDefault();
+//     this.props.onSubmit(this.state.value.toLocaleLowerCase().trim());
+//     this.setState({ value: '' });
+//   };
+
+//   render() {
+//     return (
+//       <Header>
+//         <SearchForm onSubmit={this.handleSubmit}>
+//           <Button type="submit">
+//             <FiSearch size="16px" />
+//           </Button>
+
+//           <Input
+//             type="text"
+//             autocomplete="off"
+//             autoFocus
+//             placeholder="Search images and photos"
+//             value={this.state.value}
+//             onChange={this.handleChange}
+//           />
+//         </SearchForm>
+//       </Header>
+//     );
+//   }
+// }
+
+export const Searchbar = ({ onSubmit }) => {
+  const [value, setValue] = useState('');
+
+  const handleChange = event => {
+    setValue(event.target.value);
   };
 
-  handleChange = event => {
-    this.setState({ value: event.target.value });
-  };
-
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
-    this.props.onSubmit(this.state.value.toLocaleLowerCase().trim());
-    this.setState({ value: '' });
+    onSubmit(value.toLocaleLowerCase().trim());
+    setValue('');
   };
 
-  render() {
-    return (
-      <Header>
-        <SearchForm onSubmit={this.handleSubmit}>
-          <Button type="submit">
-            <FiSearch size="16px" />
-          </Button>
+  return (
+    <Header>
+      <SearchForm onSubmit={handleSubmit}>
+        <Button type="submit">
+          <FiSearch size="16px" />
+        </Button>
 
-          <Input
-            type="text"
-            autocomplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            value={this.state.value}
-            onChange={this.handleChange}
-          />
-        </SearchForm>
-      </Header>
-    );
-  }
-}
+        <Input
+          type="text"
+          autocomplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={value}
+          onChange={handleChange}
+        />
+      </SearchForm>
+    </Header>
+  );
+};
